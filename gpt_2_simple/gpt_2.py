3a17832157f519f6cb0baa131d298d3635807ffb
ZeroMaxinumXZ <that.coder.with.cake@gmail.com>
2019-08-26 16:59:43 -0500

    Update gpt_2.py

--- gpt_2_simple/gpt_2.py
+++ gpt_2_simple/gpt_2.py
@@ -25,7 +25,7 @@ from gpt_2_simple.src import model, sample, encoder, memory_saving_gradients
 from gpt_2_simple.src.load_dataset import load_dataset, Sampler
 from gpt_2_simple.src.accumulate import AccumulatingOptimizer
 
-def mutual_info_loss(self, c, c_given_x):
+def mutual_info_loss(c, c_given_x):
     """The mutual information metric we aim to minimize"""
     eps = 1e-8
     conditional_entropy = K.mean(- K.sum(K.log(c_given_x + eps) * c, axis=1))
